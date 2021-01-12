633f965369584fc418bfcc0106760c804d4724ca
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-12 11:32:04 +0700

    Update

--- src/com/franktran/javaobjects/Example.java
+++ src/com/franktran/javaobjects/Example.java
@@ -6,6 +6,8 @@ package com.franktran.javaobjects;
 public class Example {
 
     public static void main(String[] args) {
-
+        System.out.println("We are making a new Pez Dispenser");
+        PezDispenser dispenser = new PezDispenser();
+        System.out.printf("The dispenser is %s", dispenser.name);
     }
 }
