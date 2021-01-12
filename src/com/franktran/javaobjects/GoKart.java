cbc24dbbe924332ac9606c5e759eec9b61f453bb
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-12 11:23:44 +0700

    Update

---
+++ src/com/franktran/javaobjects/GoKart.java
@@ -0,0 +1,13 @@
+package com.franktran.javaobjects;
+
+/**
+ * @author frank.tran
+ */
+public class GoKart {
+
+    private String color = "red";
+
+    public String getColor() {
+        return color;
+    }
+}
