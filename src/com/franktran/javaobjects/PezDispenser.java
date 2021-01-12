55aa5f201f728035771972c90bb719411b8ff760
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-01-12 11:19:44 +0700

    Update

---
+++ src/com/franktran/javaobjects/PezDispenser.java
@@ -0,0 +1,16 @@
+package com.franktran.javaobjects;
+
+/**
+ * @author frank.tran
+ */
+public class PezDispenser {
+
+    private String name = "Yoda";
+
+    // String.contains
+    // public boolean contains(String matchingText)
+
+    public String getName() {
+        return name;
+    }
+}
