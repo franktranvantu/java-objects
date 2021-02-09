20a8ef04582c5096854c6f698bd2814d76e4753b
Frank Tran <franktranvantu@gmail.com>
2021-02-09 22:14:29 +0700

    Update

--- final/src/com/franktran/end/GoKart.java
+++ final/src/com/franktran/end/GoKart.java
@@ -2,7 +2,7 @@
 //2. Great! Now add a field named color of type String that will be used to store the color of the GoKart.
 //Set the value to "red"
 
-package com.franktran;
+package com.franktran.end;
 
 public class GoKart {
 
