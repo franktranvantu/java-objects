8dabafd4276bb07bcc7e369cc40e239d173826ce
Frank Tran <franktranvantu@gmail.com>
2021-02-09 22:27:39 +0700

    Update

--- final/src/com/franktran/end/PezDispenser.java
+++ final/src/com/franktran/end/PezDispenser.java
@@ -1,14 +1,8 @@
 package com.franktran.end;
 
-/**
- * @author frank.tran
- */
 public class PezDispenser {
 
     private String name = "Yoda";
-
-    // String.contains
-    // public boolean contains(String matchingText)
 
     public String getName() {
         return name;
